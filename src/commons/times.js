9da6541adea5de5162515f2a3dfa403a558e72de
cantatas <zennqann@foxmail.com>
2021-05-06 09:38:56 +0800

    解决时间出现负数问题

--- src/commons/times.js
+++ src/commons/times.js
@@ -1,12 +1,14 @@
 export function timeFormat(timestamp) {
+  const GANGG = '刚刚'
   let mistiming = Math.round((Date.now() - timestamp) / 1000);
   let arrr = ['年', '个月', '星期', '天', '小时', '分钟', '秒'];
   let arrn = [31536000, 2592000, 604800, 86400, 3600, 60, 1];
   for (let i = 0; i < arrn.length; i++) {
     let inm = Math.floor(mistiming / arrn[i]);
     if (inm != 0) {
-      return inm + arrr[i] + '前';
+      const res = (inm + arrr[i]);
+      return inm < 0 ? GANGG : res + '前';
     }
   }
-  return '刚刚'
+  return GANGG
 }
